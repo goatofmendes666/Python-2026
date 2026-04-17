bcf4d04e7f4099f1a3afe04f7bb2dd48ef5bcd1b
Garv <garvagarwal97@gmail.com>
2026-04-17 18:43:51 +0530

    Added Python basics practice

---
+++ Python Basics/1.Hello World.py
@@ -0,0 +1,5 @@
+# basic print Sysntax
+print("Hello World!")
+print(5)
+print(1+2)
+print("I want", 2, "Rupees") # using "," we can cancatinate values of 2 different data types or same data type.
---
+++ Python Basics/2. Comments, Escape Sequence and Print Statement.py
@@ -0,0 +1,11 @@
+# We can print any string in python no matter how long it is, provided that It should not be in multiple lines.
+# Examples
+print("Garv is a Successful Man") # This is correct and won't give me errors.
+# Se below how using multiple lines for 1 print statement shows error.
+# The error is called "EOL" End of Line error, which means, that the line should have ended at "Man"
+## But it has not, thus --> error.  
+##
+#print("Garv is a Successful Man
+#and is also charming and hot")
+
+# Escape Characters
