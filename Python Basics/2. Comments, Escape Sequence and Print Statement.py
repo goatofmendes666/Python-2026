3f7e685fb6561aa02e007b8b439972ef759f7864
Garv <garvagarwal97@gmail.com>
2026-04-17 19:53:53 +0530

    Learned Python syntax basics, print statement variations, and escape characters (newline, double quotes)

--- Python Basics/2. Comments, Escape Sequence and Print Statement.py
+++ Python Basics/2. Comments, Escape Sequence and Print Statement.py
@@ -9,5 +9,22 @@ print("Garv is a Successful Man") # This is correct and won't give me errors.
 #and is also charming and hot")
 
 # Escape Characters
-#By using Escape characters I can achieve what we wanted.
+# By using Escape characters I can achieve what we wanted.
 print("Garv is a Successful Man \nand he is charming and hot.")
+# Note --> We can comment or uncomment multiple comment lines by using a shortcut key --> ctrl + /
+# We can also make multi-Line Comments using triple single/double inverted quotes. Eg-->
+'''
+Hello!
+How Are you?
+I am fine.
+'''
+# If you want to use Double Quotes in side strings then we use (\") for this.
+print("Hello! My name is \"Garv Garwal\" and I am \"26\" years old.")
+
+## We don't just use "Print Statement" for printing Strings
+# We can also use it for many other things like -->
+print("Hey! My name is Garv Agarwal, and I am", 26, "Years old." ) # This way we didn't used any escape characters.
+print ("Garv", 26, "Male", "Height", 5.7, sep= "~") # "Sep" seperates each value with the "~" where commas are present.
+print("Garv", 26, "Male", "Height", 5.7, sep= "~", end="Bye!\n")
+print("Hi")
+# using (end=" ") helps to end the print statement with any value specified.
