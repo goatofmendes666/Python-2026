02e162ef64f6cedd08e78b2cfc2d5bfd1c169b09
Garv <garvagarwal97@gmail.com>
2026-04-18 22:25:03 +0530

    Add Python variable basics and data type demonstration
    - Introduce variables with examples (int, string, boolean, None, float, complex)
    - Demonstrate variable assignment and reference behavior
    - Use type() to display data types
    - Include examples of sequence data types (list, tuple, dictionary)
    - Add conceptual explanation of variables as containers (learning notes)
    - Print outputs for clarity and understanding

---
+++ Python Basics/3. Variables and Data Types.py
@@ -0,0 +1,33 @@
+a = 10
+b = "Garv"
+# Variable is like a container, we can use this container to store any type of data --> String, decimal, float, double, char.
+# A variable is like a container which holds a value, just like a container holds anything liquid or solid or semi liquid
+# just like that a variable holds a data, and just like a lable gives name to the container.
+# In the same way, a variable name is a name given to the variable. 
+garv = 25
+c = garv
+print(c)
+print(a + c)
+# we can also find the datatype of a variable using "type(variabble)"
+print("The type of b is ", type(b))
+print("The type of a is", type(a)) # Integer data type
+d = True
+e = None
+f = 2.5
+g = complex(8 + 2)
+print("The type of d is", type(d)) # Boolean data type
+print("The type of e is", type(e)) # NoneType data type
+print("The type of f is", type(f)) # Float data type
+print("The type of g is", type(g)) # Complex Data Type
+
+# Special --> Sequence Data Types
+list1 = [1,5,3,8,4,6,9], [2,2.5,-5], ["apple", "banana", "qiwi", "mango", "orange"]
+print(list1) # List Data Type
+tuple1 = ((2,2.5,-4),("Apple", "banana", "mango", "orange"))
+print(tuple1) # tuple data Type
+dict1 = {"Name":"Garv", "Age": 20, "Bio": "Handsome"}
+print(dict1) # Dictionary Data Type
+
+# Let me tell you something Interesting about python.
+# In python each and everything is an object. Integer, float, boolean, None, Lists, Tupples, Dictionary, eyc. 
+# Each and everything is an object in python. 
