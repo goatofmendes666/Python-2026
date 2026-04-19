6ce5c3a73c239f62646c87098e4d95522d0b4936
Garv <garvagarwal97@gmail.com>
2026-04-20 00:37:50 +0530

    feat: add simple calculator using basic arithmetic operations
    - Perform addition, subtraction, multiplication, division
    - Include modulus and floor division operations
    - Use predefined variables for demonstration
    - Print formatted results for each operation

---
+++ Python Basics/4. Operators.py
@@ -0,0 +1,16 @@
+# We can use different operators for different purposes.
+#like for Addition, Subtraction, Multiplicatiion, Division, Modulus, Exponent, Floor Diuivision, etc
+
+# Arithmatic Operators
+a = 10
+b = 5
+c = 6
+print(a + b) # Addition operator
+print(a - b) # Subtraction operator
+print(a * b) # Multiplication operator
+print(a / b) # Division operator
+print(a % c) # Modulus operator, Remainder --> 4
+print(a % b) # Modulus operator, Remainder --> 0
+print (a // c) # Floor Division operator
+print(a ** c) # Exponent operator
+
---
+++ Python Basics/Exercise 1. Simple Calculator.py
@@ -0,0 +1,17 @@
+# This is a simple project of a simple Calculator.
+# In this we perform a simple calculations on 2 variables
+# Add, Subtract, Multiply, Divide, Modulus, Floor Division.
+a = 15
+b = 7
+add = a + b
+print("The sum of", a, "and", b, "is -->", add)
+sub = a - b
+print("The Subtraction  of", a, "and", b, "is -->", sub)
+mul = a * b
+print("The Multiplication  of", a, "and", b, "is -->", mul)
+div = a / b
+print("The Division  of", a, "and", b, "is -->", div)
+mod = a % b
+print("The Modulus  of", a, "and", b, "is -->", mod)
+fdiv = a // b
+print("The Floor Division  of", a, "and", b, "is -->", fdiv)
